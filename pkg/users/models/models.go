ef6075b8b4e4f0ef27f327999755aa8ac6a2bb9a
HuzaifaMk <76698805+HuzaifaMk@users.noreply.github.com>
2023-01-02 15:55:25 +0500

    Reverted names

--- pkg/users/models/models.go
+++ pkg/users/models/models.go
@@ -1,11 +1,11 @@
 package models
 
 type User struct {
-	ID        int64  `json:"id" gorm:"primary_key;auto_increment"`
-	FirstName string `json:"fname" gorm:"type:varchar(250)"`
-	LastName  string `json:"lname" gorm:"type:varchar(250)"`
-	Email     string `json:"email" gorm:"type:varchar(250)"`
-	Phone     int64  `json:"phone" gorm:"type:int"`
+	ID    int64  `json:"id" gorm:"primary_key;auto_increment"`
+	Fname string `json:"fname" gorm:"type:varchar(250)"`
+	Lname string `json:"lname" gorm:"type:varchar(250)"`
+	Email string `json:"email" gorm:"type:varchar(250)"`
+	Phone int64  `json:"phone" gorm:"type:int"`
 }
 
 func (b *User) TableName() string {
