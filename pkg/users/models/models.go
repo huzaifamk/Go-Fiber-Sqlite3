487a6d8810195fe4881d14b531084f8966ed7ac5
HuzaifaMk <76698805+HuzaifaMk@users.noreply.github.com>
2023-01-02 15:53:02 +0500

    Updated models JSON tag

--- pkg/users/models/models.go
+++ pkg/users/models/models.go
@@ -1,11 +1,11 @@
 package models
 
 type User struct {
-	ID    int64
-	Fname string
-	Lname string
-	Email string
-	Phone int64
+	ID    int64  `json:"id" gorm:"primary_key;auto_increment"`
+	Fname string `json:"fname" gorm:"type:varchar(250)"`
+	Lname string `json:"lname" gorm:"type:varchar(250)"`
+	Email string `json:"email" gorm:"type:varchar(250)"`
+	Phone int64  `json:"phone" gorm:"type:int"`
 }
 
 func (b *User) TableName() string {
