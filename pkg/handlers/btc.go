b2b5a4d920a19075f98416a40427ce8a1ea53119
HuzaifaMk <76698805+HuzaifaMk@users.noreply.github.com>
2023-02-01 20:36:25 +0500

    Added dollar sign

--- pkg/handlers/btc.go
+++ pkg/handlers/btc.go
@@ -14,6 +14,6 @@ func GetBTCPrice(c *fiber.Ctx) error {
 	// Convert the price to a string
 	btcPriceString := strconv.FormatFloat(btcPrice.Data.BTC.Quote.USD.Price, 'f', 2, 64)
 	// Return the price of BTC
-	return c.JSON("Bitcoin Price in USD: " + btcPriceString)
+	return c.JSON("Bitcoin Price in USD: $" + btcPriceString)
 
 }
